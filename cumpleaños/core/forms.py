82fb9786f2842a73dff8aa9ff3ac1f9001a5471e
lucaslento92 <lucaslento92@gmail.com>
2023-05-04 21:19:11 -0300

    segunda subida

--- cumpleaños/core/forms.py
+++ cumpleaños/core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class AgregarCumpleaños(forms.Form):
-    nombre = forms.CharField(label="Nombre", max_length=128)
-    apellido = forms.CharField(label="Apellido", max_length=128)
-    fecha = forms.DateField(label="Fecha", widget=forms.DateInput(attrs= {"type":"date"}))
+    nombre = forms.CharField(label="Nombre", max_length=128, required=True)
+    apellido = forms.CharField(label="Apellido", max_length=128, required=True)
+    fecha = forms.DateField(label="Fecha", widget=forms.DateInput(attrs= {"type":"date"}), required=True)
